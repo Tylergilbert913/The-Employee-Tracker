c97f2836d9c8df27cbeb6392d4af2e4b917856b9
Tyler Gilbert <tyler.gilbert913@gmail.com>
2021-03-12 15:31:06 -0500

    created role insert into seed file

--- seed.sql
+++ seed.sql
@@ -1,14 +1,41 @@
 USE employed_db;
 
-
+-- department table values
 INSERT INTO department (name)
 VALUES ("Accounting");
 
 INSERT INTO department (name)
-VALUES ("Human Resources");
+VALUES ("Legal");
 
 INSERT INTO department (name)
 VALUES ("Sales");
 
 INSERT INTO department (name)
 VALUES ("Engineering");
+
+-- role table values
+INSERT INTO role (title, salary, department_id)
+VALUES ("Accountant", 90000, 1);
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Lawyer", 150000, 2);
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Sales Consultant", 120000, 3);
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Engineer", 1150000, 4);
+
+-- employee table values
+INSERT INTO role (title, salary, department_id)
+VALUES ("Accountant", 90000, 1);
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Lawyer", 150000, 2);
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Sales Consultant", 120000, 3);
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Engineer", 1150000, 4);
+
