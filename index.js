3f6d05af5349f0b6d13735384276d09b1ac0efb6
Tyler Gilbert <tyler.gilbert913@gmail.com>
2021-03-16 00:39:31 -0400

    creating my switch and case function

--- index.js
+++ index.js
@@ -15,28 +15,53 @@ connection.connect(err => {
     if (err) throw err;
     console.log(`We connected! Connected as thread id ${connection.threadId}`);
     searchParams();
-    
+
 });
 
 function init() {
     inquirer.prompt([
         {
-        type: "list",
-        message: "What would you like to do?",
-        name: "options",
-        choices: [
-            "View all employees",
-            "View all employees by department",
-            "Add employee",
-            "Update employee role",
-            "View all roles",
-            "Add role",
-            "View all departments",
-            "Add department",
-            "Quit",
-        ]
+            type: "list",
+            message: "What would you like to do?",
+            name: "options",
+            choices: [
+                "View all employees",
+                "View all employees by department",
+                "Add employee",
+                "Update employee role",
+                "View all roles",
+                "Add role",
+                "View all departments",
+                "Add department",
+                "Quit",
+            ]
+        }
+    ]).then((response) => {
+        switch (response.choices) {
+            case "View all employees":
+                viewAllEmployees();
+                break;
+
+            case "View all employees by department":
+                employeesByDepart();
+                break;
+
+            case "Add employee":
+                addEmployee();
+                break;
+
+                case "Update employee role":
+                    updateEmployee();
+                    break;
+
+                    case "View all roles":
+                        viewAllRoles();
+                        break;
+
+                        case 
         }
 
+    }
 
 
 
